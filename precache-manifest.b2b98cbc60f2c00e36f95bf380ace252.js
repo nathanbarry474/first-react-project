b8f648c247e42bf8a61841aa1a5b5ca36a7a7482
Nathan Barry <Nathanbarry474@gmail.com>
2020-05-17 16:41:47 -0500

    Updates

---
+++ precache-manifest.b2b98cbc60f2c00e36f95bf380ace252.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37b9681362c7193b952290b9e7023dcf",
+    "url": "/first-react-project/index.html"
+  },
+  {
+    "revision": "f5791561275720e8ef8f",
+    "url": "/first-react-project/static/css/main.261a0a9c.chunk.css"
+  },
+  {
+    "revision": "10672cb1278822321a2e",
+    "url": "/first-react-project/static/js/2.325d5287.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first-react-project/static/js/2.325d5287.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5791561275720e8ef8f",
+    "url": "/first-react-project/static/js/main.3617a68b.chunk.js"
+  },
+  {
+    "revision": "d20ba613cd4a9d7d8574",
+    "url": "/first-react-project/static/js/runtime-main.ce5ebb95.js"
+  }
+]);
